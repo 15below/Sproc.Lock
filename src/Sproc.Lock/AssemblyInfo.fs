be18d2ecb6945738e8da6c3980529727d4863b01
TeamCity <github.ci@15below.com>
2015-01-05 16:49:55 +0000

    Bump version to 0.9.1

--- src/Sproc.Lock/AssemblyInfo.fs
+++ src/Sproc.Lock/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Sproc.Lock")>]
 [<assembly: AssemblyProductAttribute("Sproc.Lock")>]
 [<assembly: AssemblyDescriptionAttribute("SQL Server based distributed locking.")>]
-[<assembly: AssemblyVersionAttribute("0.9.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.0")>]
+[<assembly: AssemblyVersionAttribute("0.9.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.9.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.0"
+    let [<Literal>] Version = "0.9.1"
