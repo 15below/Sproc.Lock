db1ff96a4cc2201e74e919271ccadf31b0cf06f2
15below-OpenSource <github@15below.com>
2015-01-09 17:00:08 +0000

    Bump version to 1.0.0-alpha1

--- src/Sproc.Lock/AssemblyInfo.fs
+++ src/Sproc.Lock/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Sproc.Lock")>]
 [<assembly: AssemblyProductAttribute("Sproc.Lock")>]
 [<assembly: AssemblyDescriptionAttribute("SQL Server based distributed locking.")>]
-[<assembly: AssemblyVersionAttribute("0.9.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.2")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.2"
+    let [<Literal>] Version = "1.0.0"
